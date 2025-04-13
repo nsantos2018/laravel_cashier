3f378d3f9e1288a603c07833672812977022475a
Natanael Santos <natanael.santos.gomes@gmail.com>
2025-04-13 12:52:06 -0300

    003

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    public function loginPage()
+    {
+        return view('login');
+    }
+
+    public function loginSubmit($id)
+    {
+        // direct login
+        $user = User::findOrFail($id);
+        if($user)
+        {
+            auth()->login($user);
+            //echo "Logado com Sucesso! <br> " . auth()->user()->name;
+            return redirect()->route('plans');
+        }
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+        return redirect()->route('login');
+    }
+
+    public function plans()
+    {
+        return view('plans');
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,18 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/login', [MainController::class, 'loginPage'])->name('login');
+Route::get('/login/{id}', [MainController::class, 'loginSubmit'])->name('login.submit');
+Route::get('/logout', [MainController::class, 'logout'])->name('logout');
+Route::get('/plans', [MainController::class, 'plans'])->name('plans');
+
+/*
 Route::get('/', function () {
     //return view('welcome');
     $users = User::all();
     dd($users);
 });
+*/
