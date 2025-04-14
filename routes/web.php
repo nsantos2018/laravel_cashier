8304b8dfb3bc91f0810072494f9e9d196b4a2915
Natanael Santos <natanael.santos.gomes@gmail.com>
2025-04-13 21:02:24 -0300

    005

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\MainController;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
+
+Route::get('/', [MainController::class, 'loginPage'])->name('home');
 Route::get('/login', [MainController::class, 'loginPage'])->name('login');
 Route::get('/login/{id}', [MainController::class, 'loginSubmit'])->name('login.submit');
 Route::get('/logout', [MainController::class, 'logout'])->name('logout');
